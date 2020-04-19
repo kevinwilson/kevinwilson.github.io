393bd47294091e17258e4a12d9169db2bee743c7
Kevin Wilson (TAU) <kevin.wilson@tuni.fi>
2020-04-19 19:45:42 +0300

    Added basic offline capability.

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-// if ('serviceWorker' in navigator) {
-//     window.addEventListener('load', () => {
-//         navigator.serviceWorker.register('/service-worker.js');
-//     });
-// }
-
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/service-worker.js')
     .then((registration) => console.log('Service worker registered', registration))
--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,37 @@
+const lakanaDocumentsCache = 'lakana';
+const assets = [
+  '/',
+  '/index.html',
+  '/app.js',
+  '/lakana.ico',
+  '/styles.css',
+  '/content/sop1.html',
+  '/content/process1.html',
+  'https://unpkg.com/material-components-web@v4.0.0/dist/material-components-web.min.js',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://unpkg.com/material-components-web@v4.0.0/dist/material-components-web.min.css',
+  'https://fonts.googleapis.com/css?family=Roboto:300,400,500'
+];
+
+
 self.addEventListener('install', (event) => {
-  console.log('Service worker has been installed');
+  event.waitUntil(
+    caches.open(lakanaDocumentsCache).then((cache) => {
+      console.log('Caching assets.');
+      cache.addAll(assets);
+    })
+  );
 });
 
 self.addEventListener('activate', (event) => {
-  console.log('Service worker has been activated');
+
 });
 
 self.addEventListener('fetch', (event) => {
-  console.log('Fetch event', event);
+  event.respondWith(
+    caches.match(event.request).then((cacheResponse) => {
+      return cacheResponse || fetch(event.request);
+    }))
 });
 
 
